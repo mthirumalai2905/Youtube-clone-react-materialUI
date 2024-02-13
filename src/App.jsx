500e599ca4aae93b095e56cc3d8875a65549f641
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-02-13 15:08:32 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from '@mui/material';
 
+
 import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components';
+
 
 const App = () => (
   <BrowserRouter>
